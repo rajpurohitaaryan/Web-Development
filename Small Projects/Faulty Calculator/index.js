08e2d000784cb1f491bacaf8f05c8e047d114d2d
rajpurohitaaryan <rajpurohitaaryan429@gmail.com>
2025-05-26 01:07:02 +0530

    Add initial JavaScript implementation for Faulty Calculator project

---
+++ Small Projects/Faulty Calculator/index.js
@@ -0,0 +1,18 @@
+let a = 5
+let b = 8
+let c = Math.random()
+
+if (c > 0.1) {
+    console.log("Add-->", a - b)
+    console.log("Multiply-->", a + b)
+    console.log("Subtract-->", a / b)
+    console.log("Divide-->", a ** b)
+
+}
+else {
+    console.log("Add-->", a + b)
+    console.log("Multiply-->", a * b)
+    console.log("Subtract-->", a - b)
+    console.log("Divide-->", a / b)
+
+}
